565a94f40924e2b470858d5d4d7d925d343e9653
cttam <tamthanhcao@gmail.com>
2022-10-02 00:09:03 +0700

    test 01

--- src/index.js
+++ src/index.js
@@ -36,7 +36,7 @@ app.set('views', path.join(__dirname, 'resources', 'views'));
 // Routes init
 route(app);
 
-            app.listen(port, () => {
+             app.listen(port, () => {
     console.log(`Example app listening on port ${port}`);
 });
 
